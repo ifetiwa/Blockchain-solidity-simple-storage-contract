62454e6f01b4dad622dbb0c9d815d84cb7f83d77
ifetiwa <43348467+ifetiwa@users.noreply.github.com>
2023-03-11 14:19:41 +0100

    Create StorageFactory.sol

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+
+
+import "./SimpleStorage.sol";
+
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public{
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //Address
+        //ABI- Application Bianary index
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+        
+    }
+
+
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+       simpleStorageArray[_simpleStorageIndex].retrieve();
+        
+    }
+}
